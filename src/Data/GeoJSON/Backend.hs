f9cbe90e9983744b6b844a4874de1b612f9340f5
Markus Barenhoff <mbarenh@alios.org>
2016-05-17 13:00:15 +0200

    feature branch for #2 abstract out the backend.

---
+++ src/Data/GeoJSON/Backend.hs
@@ -0,0 +1,14 @@
+{-# LANGUAGE TypeFamilies #-}
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Data.GeoJSON.Backend
+-- Copyright   :  (C) 2016 Markus Barenhoff
+-- License     :  BSD-style (see the file LICENSE)
+-- Maintainer  :  Markus Barenhoff <mbarenh@alios.org>
+-- Stability   :  provisional
+-- Portability :  TypeFamilies
+--
+----------------------------------------------------------------------------
+module Data.GeoJSON.Backend where
+
+
